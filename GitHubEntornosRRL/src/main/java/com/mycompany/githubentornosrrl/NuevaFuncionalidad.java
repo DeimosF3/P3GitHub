a8f31779a3736c6ba83aba55576f9a689f130747
DeimosF3 <cientoundalmatas693@gmail.com>
2024-03-07 11:12:19 +0100

    nuevaFuncionalidad
    añadido fichero nuevaFuncionalidad

---
+++ GitHubEntornosRRL/src/main/java/com/mycompany/githubentornosrrl/NuevaFuncionalidad.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.githubentornosrrl;
+
+/**
+ *
+ * @author Alumno
+ */
+public class NuevaFuncionalidad {
+
+  
+    public static void main(String[] args) {
+      // Nueva funcionalidad --- RESTA ---
+         int a = 1;
+        int b = 2;
+        int resta = a - b;
+        System.out.println(resta);
+    }
+    
+}
