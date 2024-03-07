33f812e2a6543a8596b1e51d86c03bd58c5ab9c5
DeimosF3 <157919499+DeimosF3@users.noreply.github.com>
2024-03-07 11:09:33 +0100

    añadido proyecto con nueva funcionalidad

---
+++ GitHubEntornosRRL/src/main/java/com/mycompany/githubentornosrrl/Aplicacion.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubentornosrrl;
+
+/**
+ *
+ * @author Rodrigo RL
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int suma = a + b;
+        System.out.println(suma);
+        
+        // Nueva funcionalidad --- RESTA ---
+        
+        int resta = a - b;
+        System.out.println(resta);
+    }
+}
