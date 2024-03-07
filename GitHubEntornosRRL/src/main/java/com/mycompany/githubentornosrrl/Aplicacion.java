71949ec568639d2c0213034340e1847e9a1b89e8
DeimosF3 <cientoundalmatas693@gmail.com>
2024-03-07 11:01:46 +0100

    Aplicacion
    Añadido proyecto y aplicacion

---
+++ GitHubEntornosRRL/src/main/java/com/mycompany/githubentornosrrl/Aplicacion.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubentornosrrl;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int suma = a + b;
+        System.out.println(suma);
+    }
+}
